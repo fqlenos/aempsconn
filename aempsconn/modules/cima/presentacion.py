683f2b6abbbdfbf829aab8f117da8f159bc042df
fqlenos <fqlenos@protonmail.com>
2023-09-29 17:19:40 +0200

    adding new endpoints but not tested

---
+++ aempsconn/modules/cima/presentacion.py
@@ -0,0 +1,33 @@
+"""
+Module for getting information related to Presentaciones.
+"""
+
+from logging import Logger
+
+from ..base import Base
+from ...utils import Endpoint, Keys
+
+
+class Presentacion(Base):
+    def __init__(self, config) -> None:
+        super().__init__(config=config)
+
+    def add_condition(self, key: Keys.PRESENTACION.value, value) -> None:
+        """
+        Add a value for each necessary key condition.
+
+        Arguments:
+            key: key to assign as condition
+            value: value to assign to the previous key condition
+        """
+        self.conditions.update({key: value})
+        if isinstance(self.config.logger, Logger):
+            self.config.logger.info(
+                f"'{key}' with value: '{value}' successfully added to conditions."
+            )
+
+    def get_specific_endpoint(self) -> str:
+        """
+        Get the specific endpoint for the derived class.
+        """
+        return Endpoint.PRESENTACION.value
