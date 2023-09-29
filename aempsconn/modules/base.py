ef31f69cf3a1362c0bb562a85cea9f83a936f734
fqlenos <fqlenos@protonmail.com>
2023-09-29 17:20:21 +0200

    prepared for next versions where all pages can be retrieved at once

--- aempsconn/modules/base.py
+++ aempsconn/modules/base.py
@@ -6,8 +6,8 @@ import requests
 from requests import Response
 from logging import Logger
 import sys
+from multiprocessing.pool import ThreadPool
 
-import requests
 from ..utils import ConfigModel
 from ..decorators import http_res_handler
 
@@ -17,20 +17,31 @@ class Base:
         """
         Initialize the endpoints with the same config.
         """
-
         self.config: ConfigModel = config
         self.conditions = {}
 
     @http_res_handler
-    def get(self) -> Response | None:
+    def get(self, all_pages: bool = True) -> Response:
         """
         Make request based on generated conditions.
+
+        Arguments:
+            all_pages (bool): Indicates if all the pages are going to be downloaded at once or not.
         """
         if not self.conditions:
             if isinstance(self.config.logger, Logger):
                 self.config.logger.info(f"Creating request for {self.config.url}")
                 self.config.logger.warn("Conditions cannot be {}")
             sys.exit()
+
+        if "pagina" in list(self.conditions.keys()) and all_pages:
+            """
+            Checks if "pagina" condition exists in the conditions and if the all_pages argument is chosen.
+            If "pagina" exists the all_pages condition will be turned off.
+            """
+            all_pages = False
+            if isinstance(self.config.logger, Logger):
+                self.config.logger.warn("Option 'all_pages' is disabled.")
 
         query = "?"
         for key, value in self.conditions.items():
