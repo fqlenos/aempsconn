f63f77cff7a7c2810cd6ee93580af8aa962202cf
fqlenos <fqlenos@protonmail.com>
2023-10-02 00:58:33 +0200

    remove uncreated modules

--- aempsconn/modules/base.py
+++ aempsconn/modules/base.py
@@ -4,9 +4,7 @@ Base module for getting generic methods.
 
 import requests
 from requests import Response
-from logging import Logger
 import sys
-from multiprocessing.pool import ThreadPool
 
 from ..utils import ConfigModel
 from ..decorators import http_res_handler
@@ -21,35 +19,24 @@ class Base:
         self.conditions = {}
 
     @http_res_handler
-    def get(self, all_pages: bool = True) -> Response:
+    def get(self, query: str) -> Response:
         """
         Make request based on generated conditions.
 
         Arguments:
-            all_pages (bool): Indicates if all the pages are going to be downloaded at once or not.
+            query (str): conditions to add to the request.
+
+        Result:
+            Response: response of the custom HTTP Request.
         """
-        if not self.conditions:
-            if isinstance(self.config.logger, Logger):
+        if query == "":
+            if self.config.logger is not None:
                 self.config.logger.info(f"Creating request for {self.config.url}")
-                self.config.logger.warn("Conditions cannot be {}")
+                self.config.logger.warn("Conditions cannot be empty.")
             sys.exit()
 
-        if "pagina" in list(self.conditions.keys()) and all_pages:
-            """
-            Checks if "pagina" condition exists in the conditions and if the all_pages argument is chosen.
-            If "pagina" exists the all_pages condition will be turned off.
-            """
-            all_pages = False
-            if isinstance(self.config.logger, Logger):
-                self.config.logger.warn("Option 'all_pages' is disabled.")
-
-        query = "?"
-        for key, value in self.conditions.items():
-            query += f"{key}={value}&"
-        query = query[:-1]  # so it removes the last "&"
-
         self.endpoint: str = f"{self.config.url}{self.get_specific_endpoint()}{query}"
-        if isinstance(self.config.logger, Logger):
+        if self.config.logger is not None:
             self.config.logger.info(f"URL updated to: {self.endpoint}")
 
         res = requests.get(
