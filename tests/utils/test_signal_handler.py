40e2220656a26e5a17931bc7e9d1c4087470176d
fqlenos <fqlenos@protonmail.com>
2023-10-09 10:23:31 +0200

    reorder imports

--- tests/utils/test_signal_handler.py
+++ tests/utils/test_signal_handler.py
@@ -2,13 +2,14 @@
 Test signal_handler
 """
 
-import unittest
-from unittest.mock import patch
 import signal
+from unittest import TestCase
+from unittest.mock import patch
+
 from aempsconn.utils.signal_handler import signal_handler
 
 
-class TestSignalHandler(unittest.TestCase):
+class TestSignalHandler(TestCase):
     @patch("sys.exit")  # Mock the exit function
     def test_signal_handler(self, mock_exit):
         # Call the Ctrl + C with its system signal
